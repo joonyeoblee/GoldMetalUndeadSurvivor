6bdbf3b4a711f7cfea55a95ca4c0de67afc06c90
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2024-08-06 11:27:34 +0900

    fix: 무한 맵 이동시 작동 안하는 오류 수정
    무한맵이 가끔 오류가 나는 문제를 수정함

--- Assets/Scripts/Reposition.cs
+++ Assets/Scripts/Reposition.cs
@@ -5,32 +5,41 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D collition)
+    void OnTriggerExit2D(Collider2D collition)
     {
         if (!collition.CompareTag("Area")) return;
 
         // 기존 시스템 사용시 코드
-        // Vector3 playerPos = GameManager.Instance.player.transform.position;
-        // Vector3 myPos = transform.position;
+        Vector3 playerPos = GameManager.Instance.player.transform.position;
+        Vector3 myPos = transform.position;
+
+        float dirX = playerPos.x - myPos.x;
+        float dirY = playerPos.y - myPos.y;
+
+        float diffX = Mathf.Abs(dirX);
+        float diffY = Mathf.Abs(dirY);
+
+        dirX = dirX > 0 ? 1 : -1;
+        dirY = dirY > 0 ? 1 : -1;
 
         // float diffX = Mathf.Abs(playerPos.x - myPos.x);
         // float diffY = Mathf.Abs(playerPos.y - myPos.y);
 
         // Input System 사용시 코드
-        Vector3 playerDir = GameManager.Instance.player.inputVec;
-        float diffX = playerDir.x < 0 ? -1 : 1;
-        float diffY = playerDir.y < 0 ? -1 : 1;
+        // Vector3 playerDir = GameManager.Instance.player.inputVec;
+        // float diffX = playerDir.x < 0 ? -1 : 1;
+        // float diffY = playerDir.y < 0 ? -1 : 1;
 
         switch (transform.tag)
         {
             case "Ground":
                 if (diffX > diffY)
                 {
-                    transform.Translate(Vector3.right * diffX * 40);
+                    transform.Translate(Vector3.right * dirX * 40);
                 }
                 else if (diffX < diffY)
                 {
-                    transform.Translate(Vector3.up * diffY * 40);
+                    transform.Translate(Vector3.up * dirY * 40);
                 }
                 break;
             case "Enemy":
